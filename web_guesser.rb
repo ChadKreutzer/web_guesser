288b7f9facfbda738dba9970724bfa1e554e7212
Chad Kreutzer <kanthalion@gmail.com>
2017-01-04 08:59:30 +0000

    I4: colorize background

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :number, rand(100)
+set :number => rand(100), :color => 'red'
 
 get '/' do
   guess = params['guess']
   message = check_guess(guess)
-  erb :index, locals: { number: settings.number, message: message }
+  erb :index, locals: { number: settings.number, message: message, color: settings.color }
 end
 
 def check_guess(guess)
   guess = guess.to_i
   if guess == settings.number
+    settings.color = 'green'
     return "You got it right! The number was #{settings.number}"
   end
   guess > settings.number ? too_high(guess) : too_low(guess)
 end
 
 def too_high(guess)
-  guess > settings.number + 5 ? 'Way too high!' : 'Too high!'
+  if guess > settings.number + 5
+    way_off_color
+    'Way too high!'
+  else
+    off_color
+    'Too high!'
+  end
 end
 
 def too_low(guess)
-  guess < settings.number - 5 ? 'Way too low!' : 'Too low!'
+  if guess < settings.number - 5
+    way_off_color
+    'Way too low!'
+  else
+    off_color
+    'Too low!'
+  end
+end
+
+def way_off_color
+  settings.color = 'red'
+end
+
+def off_color
+  settings.color = 'pink'
 end
