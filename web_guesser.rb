b30519f946bf76ccac7d8938ba08665199ae77a1
Chad Kreutzer <kanthalion@gmail.com>
2017-01-06 10:12:30 +0000

    I4: add guess limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,27 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :number => rand(100), :color => 'red'
+set number: rand(100), color: 'red'
+@@count = 6
 
 get '/' do
+  @@count -= 1
   guess = params['guess']
-  message = check_guess(guess)
-  erb :index, locals: { number: settings.number, message: message, color: settings.color }
+  message = @@count == 0 ? lost_game : check_guess(guess)
+  erb :index, locals: { number: settings.number,
+                        message: message,
+                        color: settings.color }
 end
 
 def check_guess(guess)
   guess = guess.to_i
   if guess == settings.number
-    settings.color = 'green'
-    return "You got it right! The number was #{settings.number}"
+    return correct_guess
   end
   guess > settings.number ? too_high(guess) : too_low(guess)
+end
+
+def correct_guess
+  settings.color = 'green'
+  "You got it right! #{end_game}"
 end
 
 def too_high(guess)
   if guess > settings.number + 5
     way_off_color
-    'Way too high!'
+    "Way too high! #{@@count}"
   else
     off_color
     'Too high!'
@@ -44,4 +52,15 @@ end
 
 def off_color
   settings.color = 'pink'
+end
+
+def lost_game
+  "sorry, you took too long. #{end_game}"
+end
+
+def end_game
+  current_number = settings.number
+  settings.number = rand(100)
+  @@count = 6
+  "The number was #{current_number}\n refresh to start a new game!"
 end
